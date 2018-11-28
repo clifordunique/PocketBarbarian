c064bff878f37b04ad31f68ab0420f7f604119a6
Crackpot <jt.wagner@outlook.de>
2018-11-28 22:45:00 +0100

    Fish und Mouse animations

---
+++ Assets/Scripts/actors/enemies/movement/FishMoveController2D.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMoveController2D : MonoBehaviour {
+
+    public float seconds;
+    public float distanceUp;
+    public float waitUnderWater;
+
+    public Transform waterSurface;
+    public GameObject waterEffectIn;
+    public GameObject waterEffectOut;
+
+    private float t;
+    private Vector3 currentStartPos;
+    private Vector3 currentEndpos;
+    private Vector3 positionUp;
+    private float waterSurfaceY;
+
+    private Animator animator;
+    private bool moveUp = true;
+    private bool waterEffectPlayed = false;
+
+    private float reachedBottomTime;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        currentStartPos = transform.position;
+        currentEndpos = new Vector3(transform.position.x, transform.position.y + distanceUp, transform.position.z);
+        positionUp = currentEndpos;
+        waterSurfaceY = waterSurface.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (moveUp && t == 0F && Time.time < (reachedBottomTime + waitUnderWater)) {
+            // wait time under water
+        } else {
+            Vector2 velocity = SmoothMove();
+            Vector2 pixelPerfectVelocity = Utils.MakePixelPerfect(velocity);
+            transform.Translate(pixelPerfectVelocity);
+        }
+
+        if (moveUp) {
+            // check if surfaceBreached Up
+            if (transform.position.y > waterSurfaceY && !waterEffectPlayed) {
+                Debug.Log("Play out splash effect");
+                InstantiateEffect(waterEffectOut);
+                waterEffectPlayed = true;
+            }
+        } else {
+            // check if surfaceBreached Down
+            if (transform.position.y < waterSurfaceY && !waterEffectPlayed) {
+                Debug.Log("Play in splash effect");
+                InstantiateEffect(waterEffectIn);
+                waterEffectPlayed = true;
+            }
+        }
+    }
+
+
+    private Vector3 SmoothMove() {
+
+        if (t <= 1.0) {
+
+            t += Time.deltaTime / seconds;
+            float newTime = t;
+            if (newTime > 1) {
+                newTime = 1;
+            }
+            float easeFactor = EasingFunction.EaseInOutCubic(0.0F, 1.0F, newTime);
+            Vector3 newPosition = Vector3.Lerp(currentStartPos, currentEndpos, easeFactor);            
+
+            return newPosition - transform.position;
+
+        } else {
+            if (transform.position == positionUp) {
+                animator.SetBool("UP", false);
+                animator.SetBool("TURN", true);
+                moveUp = false;
+            } else {
+                animator.SetBool("UP", true);
+                animator.SetBool("TURN", false);
+                moveUp = true;
+                reachedBottomTime = Time.time;
+            }
+
+            t = 0F;
+            
+            Vector3 dummy = currentStartPos;
+            currentStartPos = currentEndpos;
+            currentEndpos = dummy;
+            waterEffectPlayed = false;
+        }
+        return Vector3.zero;
+    }
+
+
+
+    public void InstantiateEffect(GameObject effectToInstanciate) {
+        GameObject effect = (GameObject)Instantiate(effectToInstanciate);
+        effect.transform.parent = EffectCollection.GetInstance().transform;
+        effect.transform.position = new Vector3(transform.position.x, waterSurfaceY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/game/decoration/MouseController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    public LayerMask interactableLayer;
+    public float speed;
+    public float moveX;
+
+    private bool run = false;
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+    private float seconds;
+    private Vector3 currentStartPos;
+    private Vector3 currentEndpos;
+    private float t = 0;
+    private float startDelay;
+
+    // Use this for initialization
+    void Start () {
+        startDelay = Random.Range(0F, 2F);
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        seconds = Mathf.Abs(moveX) * (1/speed);
+        currentStartPos = transform.position;
+        currentEndpos = new Vector3(transform.position.x + moveX, transform.position.y, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!run && Time.time > startDelay) {
+            animator.SetBool("IDLE", true);
+        }
+
+        if (run) {
+            Vector2 velocity = SmoothMove();
+            Vector2 pixelPerfectVelocity = Utils.MakePixelPerfect(velocity);
+            transform.Translate(pixelPerfectVelocity);
+        }
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collider) {
+        // react to trigger
+        if (interactableLayer == (interactableLayer | (1 << collider.gameObject.layer))) {
+            animator.SetBool("RUN" ,true);
+            run = true;
+            boxCollider.enabled = false;
+        }
+    }
+
+
+    private Vector3 SmoothMove() {
+
+        if (t <= 1.0) {
+
+            t += Time.deltaTime / seconds;
+            float newTime = t;
+            if (newTime > 1) {
+                newTime = 1;
+            }
+            Debug.Log("time" + newTime);
+            float easeFactor = EasingFunction.EaseInOutSine(0.0F, 1.0F, newTime);
+            Vector3 newPosition = Vector3.Lerp(currentStartPos, currentEndpos, easeFactor);
+
+            return newPosition - transform.position;
+
+        }
+        run = false;
+        animator.SetBool("OUT", true);
+        return Vector3.zero;
+    }
+
+    public void OutEnd() {
+        Destroy(gameObject);
+    }
+
+
+}
