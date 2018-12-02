77c212c37def1046f4ba8d430fb2cf571a3a9e51
Crackpot <jt.wagner@outlook.de>
2018-12-02 23:02:40 +0100

    LockedInteractables als eigene Scripte

--- Assets/Scripts/game/interactables/AbstactInteractable.cs
+++ Assets/Scripts/game/interactables/AbstactInteractable.cs
@@ -2,129 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AbstactInteractable : MonoBehaviour {
+public abstract class AbstactInteractable: MonoBehaviour {
 
 
     public GameObject actionArrow;
-    public GameObject actionArrowLocked;
-    public GameObject lockSymbol;
-    public GameObject keySymbol;
-
     public LayerMask reactLayer;
-
-    public bool locked;
-    [ConditionalHideAttribute("locked", true)]
-    public CollectableKeys.KEY_TYPE lockedKey;
-    [ConditionalHideAttribute("locked", true)]
-    public float unlockTime = 1F;
-    public float waitAfterUnlockTime = 0.2F;
     public bool permanentDisabled = false;
-    
+
     [HideInInspector]
     public bool actionFinished = false;
 
-    private BoxCollider2D boxCollider;
-
     public virtual void Start() {
-        boxCollider = GetComponent<BoxCollider2D>();
-        if (locked) {
-            if (Unlockable()) {
-                lockSymbol.SetActive(false);
-                keySymbol.SetActive(true);
-                boxCollider.enabled = true;
-            } else {
-                lockSymbol.SetActive(true);
-                keySymbol.SetActive(false);
-                boxCollider.enabled = false;
-            }
-        } else {
-            lockSymbol.SetActive(false);
-            keySymbol.SetActive(false);
-            boxCollider.enabled = true;
-        }
-    }
 
-    public void Update() {
-        if (Time.frameCount % 5 == 0) {
-            if (locked) {
-                if (Unlockable()) {
-                    lockSymbol.SetActive(false);
-                    keySymbol.SetActive(true);
-                    boxCollider.enabled = true;
-                } else {
-                    lockSymbol.SetActive(true);
-                    keySymbol.SetActive(false);
-                    boxCollider.enabled = false;
-                }
-            }
-        }
     }
 
     public abstract void Activate();
 
-    public void Unlock() {
-        if (locked && Unlockable()) {
-            StartCoroutine(UnlockCoroutine());
-        } 
-    }
 
-    IEnumerator UnlockCoroutine() {
-        SpriteRenderer spriteRenderer = actionArrowLocked.GetComponent<SpriteRenderer>();
-        SpriteFlashingEffect effect = new SpriteFlashingEffect();
-        StartCoroutine(effect.ActionFlashing(spriteRenderer, unlockTime));
-        yield return new WaitForSeconds(unlockTime);
-        actionArrowLocked.SetActive(false);
-        yield return new WaitForSeconds(waitAfterUnlockTime);
-
-        locked = false;
-        keySymbol.SetActive(false);
-        actionArrowLocked.SetActive(false);
-        actionArrow.SetActive(true);
-    }
-
-
-    public void OnTriggerEnter2D(Collider2D collider) {
+    public virtual void OnTriggerEnter2D(Collider2D collider) {
         // react to trigger
         if (reactLayer == (reactLayer | (1 << collider.gameObject.layer))) {
             if (!permanentDisabled) {
-                if (locked) {                    
-                    if (Unlockable()) {
-                        actionArrowLocked.SetActive(true);
-                    }
-                } else {
-                    actionArrow.SetActive(true);
-                }
+                actionArrow.SetActive(true);
             }
         }
     }
 
-    public void OnTriggerExit2D(Collider2D collider) {
+    public virtual void OnTriggerExit2D(Collider2D collider) {
         // react to trigger
         if (reactLayer == (reactLayer | (1 << collider.gameObject.layer))) {
             if (!permanentDisabled) {
-                actionArrowLocked.SetActive(false);
                 actionArrow.SetActive(false);
             }
-        }
-    }
-
-    public bool Unlockable() {
-        if (locked) {
-            PlayerStatistics ps = PlayerStatistics.GetInstance();
-            if (ps) {
-                if (lockedKey == CollectableKeys.KEY_TYPE.CIRCLE && ps.hasCircleKey) {
-                    return true;
-                }
-                if (lockedKey == CollectableKeys.KEY_TYPE.SQUARE && ps.hasSquareKey) {
-                    return true;
-                }
-                if (lockedKey == CollectableKeys.KEY_TYPE.TRIANGLE && ps.hasTriangleKey) {
-                    return true;
-                }
-            }
-            return false;
-        } else {
-            return true;
         }
     }
 }
--- Assets/Scripts/game/interactables/DoorLocked.cs
+++ Assets/Scripts/game/interactables/DoorLocked.cs
@@ -3,21 +3,56 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class Door: AbstactInteractable {
+public class DoorLocked : AbstactLockedInteractable {
 
+    public DoorKeySymbols keySymbols;
     public Sprite closedDoor;
-    public Sprite openDoor;
-    public Door otherDoor;
+    public Sprite openDoor;    
+    public DoorLocked otherDoor;
     public Animator fadeAnimator;
     public Image uiImage;
-
+    public float waitOpenTime;
+ 
     [HideInInspector]
     public bool inAnimation = false;
     private SpriteRenderer spriteRenderer;
-
+ 
     public override void Start() {
         base.Start();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        if (locked && keySymbols) {
+            keySymbols.gameObject.SetActive(true);
+            keySymbols.InitKeySymbol(lockedKey);
+        }
+    }
+
+
+    public override void Activate() {
+        actionFinished = false;
+        float actualWaitOpenTime = waitOpenTime;
+        if (locked) {
+            if (Unlockable()) {
+                // nicht durch die tuer gehen sondern erst aufschliessen
+                Unlock();
+                otherDoor.locked = false;
+                otherDoor.keySymbol.SetActive(false);
+                actualWaitOpenTime += unlockTime + waitAfterUnlockTime;
+            } else {
+                // locked - keine Action
+                return;
+            }
+        }
+        StartCoroutine(WalkThroughDoor(actualWaitOpenTime));
+    }
+
+
+    IEnumerator WalkThroughDoor(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        OpenDoor();        
+        if (!inAnimation) {
+            inAnimation = true;
+            StartCoroutine(FadeOut());
+        }
     }
 
     private void OpenDoor() {
@@ -25,19 +60,10 @@ public class Door: AbstactInteractable {
     }
 
 
-    public override void Activate() {
-        OpenDoor();
-        actionFinished = false;
-        if (!inAnimation) {
-            inAnimation = true;
-            StartCoroutine(FadeOut());
-        }
-    }
-
     IEnumerator FadeOut() {
         fadeAnimator.SetBool("FADE_IN", false);
         fadeAnimator.SetBool("FADE_OUT", true);
-        yield return new WaitUntil(() => uiImage.color.a == 1);
+        yield return new WaitUntil(()=> uiImage.color.a == 1);
         Teleport();
     }
 
